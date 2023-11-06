3aa4f306a443d48a281caeb8dd077180e3636bfb
Rozerin Kasman <rozerin.kasman@infoera.com.tr>
2023-11-06 16:46:47 +0300

    simple login-register

---
+++ TaskManegementSystem/Areas/Identity/Data/TaskManegementDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManegementSystem.Areas.Identity.Data;
+
+namespace TaskManegementSystem.Data;
+
+public class TaskManegementDbContext : IdentityDbContext<TaskManegementSystemUser>
+{
+    public TaskManegementDbContext(DbContextOptions<TaskManegementDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserIdentityConfiguration());
+    }
+    public class ApplicationUserIdentityConfiguration: IEntityTypeConfiguration<TaskManegementSystemUser>
+    {
+        
+
+        public void Configure(EntityTypeBuilder<TaskManegementSystemUser> builder)
+        {
+            builder.Property(u => u.FirstName).HasMaxLength(250);
+            builder.Property(u => u.LastName).HasMaxLength(250);
+        }
+    }
+}
--- TaskManegementSystem/Program.cs
+++ TaskManegementSystem/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TaskManegementSystem.Areas.Identity.Data;
+using TaskManegementSystem.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TaskManegementDbContextConnection") ?? throw new InvalidOperationException("Connection string 'TaskManegementDbContextConnection' not found.");
+
+builder.Services.AddDbContext<TaskManegementDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<TaskManegementSystemUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<TaskManegementDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -13,11 +25,13 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();

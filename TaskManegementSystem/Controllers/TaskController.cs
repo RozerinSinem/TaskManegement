66ed9a3778335ae0ad17b79f881d831c213b4b87
Rozerin Kasman <rozerin.kasman@infoera.com.tr>
2023-11-07 16:50:52 +0300

    default crud operations withot create and all task page

--- TaskManegementSystem/Controllers/TaskController.cs
+++ TaskManegementSystem/Controllers/TaskController.cs
@@ -1,49 +1,149 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using TaskManegementSystem.Data;
 using TaskManegementSystem.Models;
 
 namespace TaskManegementSystem.Controllers
-
 {
     public class TaskController : Controller
     {
-        private readonly TaskManegementDbContext _db;
+        private readonly TaskManegementDbContext _context;
 
-        public TaskController (TaskManegementDbContext db)
+        public TaskController(TaskManegementDbContext context)
         {
-            _db = db;
+            _context = context;
         }
 
-
-        [HttpGet]
-        public IActionResult CreateTask()
+        // GET: Task
+        public async Task<IActionResult> Index()
         {
-            return View();
+              return _context.Tasks != null ? 
+                          View(await _context.Tasks.ToListAsync()) :
+                          Problem("Entity set 'TaskManegementDbContext.Tasks'  is null.");
         }
 
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult CreateTask(TaskViewModel obj)
+        // GET: Task/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
-
-            if(ModelState.IsValid)
+            if (id == null || _context.Tasks == null)
             {
-
-                
-                _db.Tasks.Add(obj);
-                _db.SaveChanges();
-                return RedirectToAction("Success");
+                return NotFound();
             }
 
-            return View(obj);
+            var taskViewModel = await _context.Tasks
+                .FirstOrDefaultAsync(m => m.TaskID == id);
+            if (taskViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(taskViewModel);
         }
 
-        public IActionResult Success()
+        // GET: Task/Create
+        
+
+        // POST: Task/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+       
+
+        // GET: Task/Edit/5
+        public async Task<IActionResult> Edit(int? id)
         {
+            if (id == null || _context.Tasks == null)
+            {
+                return NotFound();
+            }
 
-          return View();
+            var taskViewModel = await _context.Tasks.FindAsync(id);
+            if (taskViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(taskViewModel);
         }
 
+        // POST: Task/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TaskID,Name,Description,AssignedUser")] TaskViewModel taskViewModel)
+        {
+            if (id != taskViewModel.TaskID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(taskViewModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TaskViewModelExists(taskViewModel.TaskID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(taskViewModel);
+        }
+
+        // GET: Task/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Tasks == null)
+            {
+                return NotFound();
+            }
+
+            var taskViewModel = await _context.Tasks
+                .FirstOrDefaultAsync(m => m.TaskID == id);
+            if (taskViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(taskViewModel);
+        }
+
+        // POST: Task/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Tasks == null)
+            {
+                return Problem("Entity set 'TaskManegementDbContext.Tasks'  is null.");
+            }
+            var taskViewModel = await _context.Tasks.FindAsync(id);
+            if (taskViewModel != null)
+            {
+                _context.Tasks.Remove(taskViewModel);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TaskViewModelExists(int id)
+        {
+          return (_context.Tasks?.Any(e => e.TaskID == id)).GetValueOrDefault();
+        }
     }
 }
